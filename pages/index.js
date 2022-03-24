b763c22610941f4325cc2fc370a0df56dd5a9c4a
Kent Miguel <kentmiguel10@gmail.com>
2022-03-24 19:40:24 -0400

    BaseLayout component
    Created a Layout Component for Navbar and footer

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,18 @@
-import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common";
+import { Hero, Breadcrumbs } from "@components/common";
 import { OrderCard } from "@components/common/order";
 import { CourseList } from "@components/course";
 import { EthRates, WalletBar } from "@components/web3";
+import { BaseLayout } from "@components/layout";
 
 export default function Home() {
   return (
-    <div>
-      <div className="relative bg-white overflow-hidden">
-        <div className="relative max-w-7xl mx-auto px-4">
-          <Navbar />
-          <div className="fit">
-            <Hero />
-            <Breadcrumbs />
-            <WalletBar />
-            <EthRates />
-            <OrderCard />
-            <CourseList />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </div>
+    <BaseLayout>
+      <Hero />
+      <Breadcrumbs />
+      <WalletBar />
+      <EthRates />
+      <OrderCard />
+      <CourseList />
+    </BaseLayout>
   );
 }
