5c67385dff20be9ab19409f25cd0c6fea3b8896d
Kent Miguel <kentmiguel10@gmail.com>
2022-03-25 08:08:17 -0400

    Get Single Course
    Customized data for each course

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,12 +1,14 @@
 import { Modal } from "@components/common";
 import { CourseHero, Curriculum, Keypoints } from "@components/course";
 import { BaseLayout } from "@components/layout";
+import { getAllCourses } from "content/courses/fetcher";
 
-export default function Course() {
+export default function Course({ course }) {
   return (
     <>
       <div className="py-4">
         {" "}
+        {course.title}
         <CourseHero />
       </div>
       <Keypoints />
@@ -14,6 +16,33 @@ export default function Course() {
       <Modal />
     </>
   );
+}
+
+export function getStaticPaths() {
+  //get courses
+  const { data } = getAllCourses();
+
+  //Transforms [] of obj into [] of params
+  return {
+    paths: data.map((c) => ({
+      params: {
+        slug: c.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+//returns course data
+export function getStaticProps({ params }) {
+  const { data, courseMap } = getAllCourses();
+  //Filter for specific course - compare to params
+  const course = data.filter((c) => c.slug === params.slug)[0]; //[0] because an [] is returned with 1 item
+  return {
+    props: {
+      course,
+    },
+  };
 }
 
 Course.Layout = BaseLayout;
