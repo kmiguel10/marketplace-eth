95cce4f98e526961a7bba589db484d5ab9da98c9
Kent Miguel <kentmiguel10@gmail.com>
2022-03-27 11:06:10 -0400

    Disable Button

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -8,7 +8,7 @@ export default function Navbar() {
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
-          <div className="flex justify-between">
+          <div className="flex justify-between item-center">
             <div>
               <Link href="/">
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
@@ -33,7 +33,9 @@ export default function Navbar() {
                 </a>
               </Link>
               {isLoading ? (
-                <Button onClick={connect}>Loading ... </Button>
+                <Button disabled={true} onClick={connect}>
+                  Loading ...{" "}
+                </Button>
               ) : isWeb3Loaded ? (
                 <Button onClick={connect}>Connect</Button>
               ) : (
