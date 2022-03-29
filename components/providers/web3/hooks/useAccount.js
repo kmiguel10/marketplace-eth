92117fdd982dab8058205b6a31feba37c76cfe12
Kent Miguel <kentmiguel10@gmail.com>
2022-03-29 08:08:25 -0400

    get account

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
+
 //returns a function
 export const handler = (web3) => () => {
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+
+    web3 && getAccount(); //Call get account if there is web3
+  }, [web3]); //re-render when web3 is changed
+
   return {
-    account: web3 ? "Test Account" : "null",
+    account,
   };
 };
