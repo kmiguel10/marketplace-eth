3145e7f02fe0702577e57c9843581473f4e6ab12
Kent Miguel <kentmiguel10@gmail.com>
2022-03-30 08:29:30 -0400

    Display current network
    used useSWR and useEffect to mutate chain and rerender useSWR to display the current network
    
    NOTE: use window.ethereum.on instead of provider.on for refresh to work

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,7 +1,43 @@
+import { useEffect } from "react";
+import useSWR from "swr";
+
+const NETWORKS = {
+  1: "Ethereum Main Network",
+  3: "Ropsten Test Network",
+  4: "Rinkeby Test Network",
+  5: "Goerli Test Network",
+  42: "Kovan Test Network",
+  56: "Binance Smart Chain",
+  1337: "Ganache",
+};
+
 export const handler = (web3, provider) => () => {
+  //using useWSR:
+  //1. Create a call back function
+  //2. check if web3 exist, if it does then create the name identifier
+  //3. create an asyn function to fetch the network id
+  const { mutate, ...rest } = useSWR(
+    () => (web3 ? "web3/network" : null),
+    async () => {
+      const chainId = await web3.eth.getChainId();
+      return NETWORKS[chainId];
+    }
+  );
+
+  //use useEffect in order to listen for network change and refresh the page
+  //check for provider exist and use chainChanged event from metamask and mutate netId
+  //extract mutate function from swrResponce where it changes the data (...rest) we'll be receing from it
+  useEffect(() => {
+    window.ethereum &&
+      window.ethereum.on("chainChanged", (chainId) => {
+        mutate(NETWORKS[parseInt(chainId, 16)]);
+      });
+  }, [web3]);
+
   return {
     network: {
-      data: "Testing Network",
+      mutate,
+      ...rest,
     },
   };
 };
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -11,10 +11,8 @@ export default function Marketplace({ courses }) {
   return (
     <>
       <div className="py-4">
-        {network.data}
-        <WalletBar address={account.data} />
+        <WalletBar address={account.data} network={network.data} />
       </div>
-
       <CourseList courses={courses} />
     </>
   );
