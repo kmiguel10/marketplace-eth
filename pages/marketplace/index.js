0cb91c1571934cd9618ead2fb7d9761650db3c9f
Kent Miguel <kentmiguel10@gmail.com>
2022-03-31 08:17:30 -0400

    Order Modal
    Activated modal and refactored code

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -4,6 +4,7 @@ import { getAllCourses } from "content/courses/fetcher";
 import { WalletBar } from "@components/ui/web3";
 import { useAccount, useNetwork } from "@components/hooks/web3";
 import { Button } from "@components/ui/common";
+import { OrderModal } from "@components/ui/order";
 
 export default function Marketplace({ courses }) {
   const { account } = useAccount(); //custom hook
@@ -34,6 +35,7 @@ export default function Marketplace({ courses }) {
           />
         )}
       </CourseList>
+      <OrderModal />
     </>
   );
 }
