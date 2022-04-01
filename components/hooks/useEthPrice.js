85a2fd674c9d95577a917be60c75c3a6b8fc8edf
Kent Miguel <kentmiguel10@gmail.com>
2022-04-01 07:46:09 -0400

    Display Price per course

--- components/hooks/useEthPrice.js
+++ components/hooks/useEthPrice.js
@@ -2,6 +2,7 @@ import useSWR from "swr";
 
 const URL =
   "https://api.coingecko.com/api/v3/coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=false";
+const COURSE_PRICE = 15;
 
 const fetcher = async (url) => {
   const res = await fetch(url);
@@ -10,6 +11,8 @@ const fetcher = async (url) => {
 };
 
 export const useEthPrice = () => {
-  const swrRes = useSWR(URL, fetcher, { refreshInterval: 1000 });
-  return { eth: swrRes };
+  const { data, ...rest } = useSWR(URL, fetcher, { refreshInterval: 10000 });
+
+  const perItem = (data && (COURSE_PRICE / Number(data)).toFixed(6)) ?? null;
+  return { eth: { data, perItem, ...rest } };
 };
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -27,7 +27,7 @@ export default function Marketplace({ courses }) {
             hasInitialResponse: network.hasInitialResponse,
           }}
         />
-        <EthRates eth={eth.data} />
+        <EthRates eth={eth.data} ethPerItem={eth.perItem} />
       </div>
       <CourseList courses={courses}>
         {(course) => (
