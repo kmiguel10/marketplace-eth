bd4ac718c0ed9c8751f8c0920b0e50e08d705666
Kent Miguel <kentmiguel10@gmail.com>
2022-04-03 15:05:32 -0400

    Created Loader component

---
+++ components/ui/common/loader/index.js
@@ -0,0 +1,10 @@
+export default function Loader() {
+  return (
+    <>
+      <div className="spinner">
+        <div className="cube1"></div>
+        <div className="cube2"></div>
+      </div>
+    </>
+  );
+}
