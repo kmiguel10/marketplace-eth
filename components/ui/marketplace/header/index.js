85d1916d26ba41ccf36c47ebe6a71c1dc1810bf9
Kent Miguel <kentmiguel10@gmail.com>
2022-04-03 09:51:25 -0400

    Breadcrumbs
    1. Create marketplace pages
    2. Navigate to these pages on the links from the breadcrumbs

--- components/ui/marketplace/header/index.js
+++ components/ui/marketplace/header/index.js
@@ -1,6 +1,21 @@
 import { Breadcrumbs } from "@components/ui/common";
 import { EthRates, WalletBar } from "@components/ui/web3";
 
+const LINKS = [
+  {
+    href: "/marketplace",
+    value: "Buy",
+  },
+  {
+    href: "/marketplace/courses/owned",
+    value: "My Courses",
+  },
+  {
+    href: "/marketplace/courses/manage",
+    value: "Manage Courses",
+  },
+];
+
 export default function Header() {
   return (
     <>
@@ -8,7 +23,7 @@ export default function Header() {
       <EthRates />
       <div className="flex flex-row-reverse pb-4 px-4 sm:px-6 lg:px-8">
         {" "}
-        <Breadcrumbs />
+        <Breadcrumbs items={LINKS} />
       </div>
     </>
   );
