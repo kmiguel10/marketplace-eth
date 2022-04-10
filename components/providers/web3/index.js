efaa26e673add52386724eb90c0e124ae625856e
Kent Miguel <kentmiguel10@gmail.com>
2022-04-10 19:01:16 -0400

    Change Load Contract implementation
    Used web3 instead of Provider and truffle/contract

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -31,7 +31,7 @@ export default function Web3Provider({ children }) {
       if (provider) {
         //if provider is detected in the browser then initialized web3Api
         const web3 = new Web3(provider); //set web3
-        const contract = await loadContract("CourseMarketplace", provider);
+        const contract = await loadContract("CourseMarketplace", web3);
         console.log(contract);
         setWeb3Api({
           provider,
