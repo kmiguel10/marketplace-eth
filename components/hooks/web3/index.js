d43ddf66cc1b56ce386bffb0a65a65b8033df3a4
Kent Miguel <kentmiguel10@gmail.com>
2022-04-26 20:52:08 -0400

    Check is empty
    Continuation

--- components/hooks/web3/index.js
+++ components/hooks/web3/index.js
@@ -1,7 +1,19 @@
 import { useHooks } from "@components/providers/web3";
 
+const _isEmpty = (data) => {
+  return (
+    data == null ||
+    data === "" ||
+    (Array.isArray(data) && data.length === 0) ||
+    (data.constructor === Object && Object.keys(data).length === 0)
+  );
+};
+
 const enhanceHook = (swrRes) => {
-  return { ...swrRes, hasInitialResponse: swrRes.data || swrRes.error };
+  const { data, error } = swrRes;
+  const hasInitialResponse = !!(data || error);
+  const isEmpty = hasInitialResponse && _isEmpty(data);
+  return { ...swrRes, isEmpty, hasInitialResponse };
 };
 
 export const useNetwork = () => {
--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -16,19 +16,18 @@ export default function OwnedCourses({ courses }) {
     <>
       <MarketHeader />
       <section className="grid grid-cols-1">
-        {ownedCourses.hasInitialResponse &&
-          (!ownedCourses.data || ownedCourses?.data.length === 0) && (
-            <div>
-              <Message type="warning">
-                <div> You do not own any courses</div>
-                <Link href="/marketplace">
-                  <a className="font-normal hover:underline">
-                    <i>Purchase Course</i>
-                  </a>
-                </Link>
-              </Message>
-            </div>
-          )}
+        {ownedCourses.isEmpty && (
+          <div className="w-1/2">
+            <Message type="warning">
+              <div>You dont own any courses</div>
+              <Link href="/marketplace">
+                <a className="font-normal hover:underline">
+                  <i>Purchase Course</i>
+                </a>
+              </Link>
+            </Message>
+          </div>
+        )}
         {ownedCourses.data?.map((course) => (
           <OwnedCourseCard key={course.id} course={course}>
             <Button onClick={() => router.push(`/courses/${course.slug}`)}>
